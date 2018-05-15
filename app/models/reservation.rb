f97c637a3615b2653be1ec718cbe0d8b86697b04
Maggie Summers <summers.maggie@gmail.com>
2018-05-15 15:36:18 -0700

    make sure database and permissions are working correctly

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :user
+
+  validates :name, :presence => true
+  validates :notes, :presence => true
+  validates :start_time, :presence => true
+  validates :end_time, :presence => true
 end
